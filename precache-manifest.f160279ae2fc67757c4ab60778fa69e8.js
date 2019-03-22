0b33b6ac48029d294b7126b90a42bc78d0a50c5c
skaldat1 <skaldat1@binghamton.edu>
2019-03-22 09:23:48 -0400

    Updates

---
+++ precache-manifest.f160279ae2fc67757c4ab60778fa69e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7af0a2dadcd6e86313d2431735f07cbc",
+    "url": "/atlas-app/static/media/Sort.7af0a2da.svg"
+  },
+  {
+    "revision": "83b6e86aebe7bbdf76fb03500c7c2401",
+    "url": "/atlas-app/static/media/CollapsePanel.83b6e86a.svg"
+  },
+  {
+    "revision": "35f6ff3aa7adf2e90683",
+    "url": "/atlas-app/static/js/runtime~main.2ddb400f.js"
+  },
+  {
+    "revision": "8359e3bacb9a77c1b92b",
+    "url": "/atlas-app/static/js/main.2d074c7d.chunk.js"
+  },
+  {
+    "revision": "4a08481007db215ad269",
+    "url": "/atlas-app/static/js/2.5375c2bc.chunk.js"
+  },
+  {
+    "revision": "8359e3bacb9a77c1b92b",
+    "url": "/atlas-app/static/css/main.5e137873.chunk.css"
+  },
+  {
+    "revision": "a075da7ddf8d8ba4fa2acf630ca42110",
+    "url": "/atlas-app/index.html"
+  }
+];
